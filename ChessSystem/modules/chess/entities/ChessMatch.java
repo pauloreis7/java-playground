5be5ffa13fbd952c3eb4b36f2a4deada0bd29565
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-03-11 11:16:30 -0300

    feat(ChessSystem): create Bishop class Piece

--- ChessSystem/modules/chess/entities/ChessMatch.java
+++ ChessSystem/modules/chess/entities/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import ChessSystem.modules.boardGame.entities.Board;
 import ChessSystem.modules.boardGame.entities.Piece;
 import ChessSystem.modules.boardGame.entities.Position;
+import ChessSystem.modules.chess.entities.pieces.Bishop;
 import ChessSystem.modules.chess.entities.pieces.King;
 import ChessSystem.modules.chess.entities.pieces.Pawn;
 import ChessSystem.modules.chess.entities.pieces.Rook;
@@ -219,7 +220,9 @@ public class ChessMatch {
 
   private void initialSetup() {
     placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+    placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
     placeNewPiece('e', 1, new King(board, Color.WHITE));
+    placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
     placeNewPiece('h', 1, new Rook(board, Color.WHITE));
     placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
     placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -231,7 +234,9 @@ public class ChessMatch {
     placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
     placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+    placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
     placeNewPiece('e', 8, new King(board, Color.BLACK));
+    placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
     placeNewPiece('h', 8, new Rook(board, Color.BLACK));
     placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
     placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
