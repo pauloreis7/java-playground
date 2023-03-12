3d3f8d25bd1a5685be7609f647bfefbb56c5df07
Paulo Reis <paulosilvadosreis2057@gmail.com>
2023-03-12 10:03:50 -0300

    refact(ChessSystem): handling better treatment to promotion move

--- ChessSystem/modules/chess/entities/ChessMatch.java
+++ ChessSystem/modules/chess/entities/ChessMatch.java
@@ -1,6 +1,5 @@
 package ChessSystem.modules.chess.entities;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -134,7 +133,7 @@ public class ChessMatch {
     }
 
     if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-      throw new InvalidParameterException("Invalid type for promotion");
+      return promoted;
     }
 
     Position pos = promoted.getChessPosition().toPosition();
